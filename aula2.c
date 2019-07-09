145198eea785aa94ae89102302460b5919d6e5bb
Jonas Wendel <jonas_wendel@usp.br>
2019-07-09 17:09:32 -0300

    aula2

---
+++ aula2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    char letra = 'a';  
+    char nro =  10;
+    int  numero = 10;
+    float n = 5.25;
+    double d = 5.0e3;
+
+    int nro1 = 10; //posso declarar assim
+    int nro2;
+    int nro3;
+    int nro1 = 10, nro2, nro3; //ou entao declarar assim
+
+    return 0;
+
+}
