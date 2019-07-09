3f749c59126cb48a160442b42de0bc611943b011
Jonas Wendel <jonas_wendel@usp.br>
2019-07-09 19:44:03 -0300

    aula5.c

---
+++ aula5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+    /* int x = 5, y;
+    y = x;
+    y = x + 10;
+  
+    int z;
+    x = y = z = 10;
+        
+    x = 97;
+    char ch1, ch2 = 'A';
+    ch1 = x;
+    printf("ch1 = %c \n",ch1);
+    x = ch2;
+    printf("x = %d \n",x);*/
+
+     /*  int x = 833;
+    float f1, f2 = 5.25;
+    f1 = x;
+    printf("f1 = %f \n",f1);
+    x = f2;
+    printf("x = %d \n",x);*/
+
+    float f = 97.3;
+    char ch1, ch2 = 'A';
+    ch1 = f;
+    printf("ch1 = %c \n",ch1);
+    f = ch2;
+    printf("f = %f \n",f);
+
+    return 0;
+}
